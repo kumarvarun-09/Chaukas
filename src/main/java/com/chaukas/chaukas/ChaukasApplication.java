4a27752346caf2744a1b9cf0361149274020bfc4
Varun Kumar <varunkr2109@gmail.com>
2026-08-17 02:15:07 +0530

    added Liquidbase

---
+++ src/main/java/com/chaukas/chaukas/ChaukasApplication.java
@@ -0,0 +1,13 @@
+package com.chaukas.chaukas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaukasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaukasApplication.class, args);
+	}
+
+}
