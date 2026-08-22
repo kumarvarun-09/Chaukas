69bd41d04cffe4d655d7bc4274a7216ed741d1c5
Varun Kumar <varunkr2109@gmail.com>
2026-08-23 01:29:02 +0530

    MonitorResponse created

--- src/main/java/com/chaukas/monitor/dto/MonitorResponse.java
+++ src/main/java/com/chaukas/monitor/dto/MonitorResponse.java
@@ -1,4 +1,25 @@
 package com.chaukas.monitor.dto;
 
-public class MonitorResponse {
+import com.chaukas.monitor.MonitorStatus;
+
+import java.time.Instant;
+
+public record MonitorResponse(
+        Long monitorId,
+        Long currentConfigId,
+        MonitorStatus status,
+        Integer consecutiveFailures,
+        boolean enabled,
+        Instant createdAt,
+        Instant lastUpdatedAt,
+        Instant nextCheckAt,
+        Integer version,
+        String name,
+        String url,
+        Integer expectedStatusCode,
+        Long timeoutInMillis,
+        Long checkIntervalInSeconds,
+        Integer failureThreshold,
+        Integer reminderAfterFailures
+) {
 }
